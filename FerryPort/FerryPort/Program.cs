1f0faaedf881b06ab7d9cb5806b65e1ac129d75c
IvanIDotien <150675981+IvanIDotien@users.noreply.github.com>
2024-02-05 15:11:07 +0100

    Add new icons and change styling

--- FerryPort/FerryPort/Program.cs
+++ FerryPort/FerryPort/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace FerryPort
@@ -23,74 +20,6 @@ namespace FerryPort
 
             window.ShowDialog();
 
-            List<Vehicle> vehicles = new List<Vehicle>();
-
-            Vehicle vehicle = new Vehicle();
-            Ferry smallFerry = new Ferry();
-            Ferry largeFerry = new Ferry();
-
-            TerminalClerk clerk = new TerminalClerk();
-
-            bool isGoodFuelLevel = true;
-            bool canOnboard = false;
-
-            string loop = "gameOn";
-            /*while (loop != "e")
-            {               
-                // Store the value of the random vehicle from the list to the vehicle object
-                vehicle = RandomVehicle(vehicles);
-                vehicle.RandomizeFuelLevel();
-
-                int fuelLevel = vehicle.GetFuelInPercentage();
-                string vehicleType = vehicle.GetVehicleType();
-
-                Console.WriteLine($"Vehicle type {vehicleType}");
-
-                if (smallFerry.GetCapacity() == 0)
-                    smallFerry = new SmallFerry();
-
-                if (largeFerry.GetCapacity() == 0)
-                    largeFerry = new LargeFerry();
-
-                Console.WriteLine($"Fuel level is on {fuelLevel}%");
-                if (clerk.NeedsRefuel(fuelLevel))
-                {
-                    isGoodFuelLevel = false;
-
-                    vehicle.RefuelTank(clerk.NewAmountOfFuel(vehicle));
-                    Console.WriteLine("Tank refueled");
-                    Console.WriteLine($"Fuel level is on {vehicle.GetFuelInPercentage()}%");
-                    isGoodFuelLevel = true;
-                    canOnboard = true;
-                }
-                else
-                {
-                    canOnboard = true;
-                }
-
-                if(isGoodFuelLevel && canOnboard)
-                {
-                    // Door manipulation
-                    clerk.CargoInspection(vehicle);
-
-                    // Small ferry
-                    if (vehicleType == "car" || vehicleType == "van")
-                    {
-                        Onboarding(vehicle, smallFerry, clerk);
-                    }
-                    // Large ferry
-                    else if (vehicleType == "bus" || vehicleType == "truck")
-                    {
-                        Onboarding(vehicle, largeFerry, clerk);
-                    }
-                }
-
-                Console.WriteLine("\n");
-
-                clerk.ShowIncome();
-                loop = Console.ReadLine();
-            }
-            */
         }
     }
 }
